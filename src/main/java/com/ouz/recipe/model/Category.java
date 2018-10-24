18aacb24162c1a1c72d43b1a6416536bcb5fc1e2
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2018-10-24 20:00:27 +0300

    Added Category and Recipe join table.

---
+++ src/main/java/com/ouz/recipe/model/Category.java
@@ -0,0 +1,50 @@
+package com.ouz.recipe.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ * @author oguz, created on 2018.10.24
+ *
+ */
+
+@Entity
+public class Category {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String description;
+
+	@ManyToMany (mappedBy = "categories")
+	private Set<Recipe> recipes;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(Set<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+}
