2e33a805f498f3b30bed83052fb15fee5863768f
Oguz Ozkeroglu <oguzozkeroglu@gmail.com>
2018-11-09 14:08:53 +0200

    Added ArgumentCaptor into IndexControllerTest class.

--- src/test/java/com/ouz/recipe/controller/IndexControllerTest.java
+++ src/test/java/com/ouz/recipe/controller/IndexControllerTest.java
@@ -1,14 +1,22 @@
 package com.ouz.recipe.controller;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
+import com.ouz.recipe.model.Recipe;
 import com.ouz.recipe.service.RecipeService;
 
 /**
@@ -33,10 +41,25 @@ public class IndexControllerTest {
 
 	@Test
 	public void testGetIndexPage() {
+		// given
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		recipes.add(new Recipe());
+
+		when(recipeService.getRecipes()).thenReturn(recipes);
+
+		@SuppressWarnings("unchecked")
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+		// when
 		String viewName = controller.getIndexPage(model);
 
+		// then
 		assertEquals("index", viewName);
 		verify(recipeService, times(1)).getRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		Set<Recipe> setInController = argumentCaptor.getValue();
+
+		assertEquals(2, setInController.size());
 	}
 }
